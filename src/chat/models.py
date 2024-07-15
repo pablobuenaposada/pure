727ce005b944a924dfefe1fabab073bef0732a4b
pablobuenaposada <pablo.buenaposada@gmail.com>
2024-07-15 23:16:05 +0200

    add user constraint to Chat model

--- src/chat/models.py
+++ src/chat/models.py
@@ -7,6 +7,7 @@ from django_extensions.db.models import TimeStampedModel
 
 class Chat(TimeStampedModel):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
 
 
 class Message(TimeStampedModel):
--- src/tests/chat/test_models.py
+++ src/tests/chat/test_models.py
@@ -2,18 +2,36 @@ import pytest
 from chat.models import Chat, Message
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
+from django.db import IntegrityError
 from model_bakery import baker
 
 
 @pytest.mark.django_db
 class TestChat:
+    def test_mandatory_fields(self):
+        """User field is mandatory"""
+        with pytest.raises(IntegrityError) as error:
+            Chat.objects.create()
+
+        assert "user_id" in error.value.args[0]
+
+    def test_chat_unique_user(self):
+        """Only one chat with specific user is allowed"""
+        user = baker.make(User)
+        Chat.objects.create(user=user)
+
+        with pytest.raises(IntegrityError):
+            Chat.objects.create(user=user)
+
     def test_valid(self):
-        chat = Chat.objects.create()
+        user = baker.make(User)
+        chat = Chat.objects.create(user=user)
         expected = {
             "created": chat.created,
             "modified": chat.modified,
             "id": chat.id,
             "messages": chat.messages,
+            "user": user,
         }
 
         for field in [field.name for field in Chat._meta.get_fields()]:
